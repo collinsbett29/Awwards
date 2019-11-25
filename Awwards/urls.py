1024f1f6a45539e1350e5dfd5b1b0c0a04fee8a6
collinsbett29 <collinsbett29@gmail.com>
2019-11-25 13:31:32 +0300

    Import admin

--- Awwards/urls.py
+++ Awwards/urls.py
@@ -1,5 +1,4 @@
-"""Awwards URL Configuration
-
+"""awards URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
@@ -15,12 +14,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('sites.urls')),
+    url(r'^', include('sites.urls')), 
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/accounts/login/'}),
-    url(r'^api/', include('sites.api.urls', namespace='api-sites'))
-
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    url(r'^api-token-auth/', obtain_auth_token)
 ]
